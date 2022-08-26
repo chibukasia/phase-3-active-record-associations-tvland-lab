03dcf13b4ce5bdc016d8d55270b4e0303b1009a2
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-26 13:57:20 +0300

    commit tree 3857c537916ee83ca0f69ede3809afa3b03debb0

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    @actors_list = []
+    self.actors.each do |actor|
+        full_name = "#{actor.first_name} #{actor.last_name}"
+        @actors_list << full_name
+    end
+    @actors_list
+  end
 end
